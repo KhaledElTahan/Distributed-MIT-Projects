9890df9fa2760daac637aae7baf9b8ab6f8392b7
Khaled El-Tahan <millerzbusiness@gmail.com>
2017-03-05 21:37:06 +0200

    Pass Lab2 Part I

--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -1,5 +1,11 @@
 package mapreduce
 
+import (
+	"os"
+	"encoding/json"
+	"sort"
+)
+
 // doReduce manages one reduce task: it reads the intermediate
 // key/value pairs (produced by the map phase) for this task, sorts the
 // intermediate key/value pairs by key, calls the user-defined reduce function
@@ -11,36 +17,36 @@ func doReduce(
 	nMap int, // the number of map tasks that were run ("M" in the paper)
 	reduceF func(key string, values []string) string,
 ) {
-	//
-	// You will need to write this function.
-	//
-	// You'll need to read one intermediate file from each map task;
-	// reduceName(jobName, m, reduceTaskNumber) yields the file
-	// name from map task m.
-	//
-	// Your doMap() encoded the key/value pairs in the intermediate
-	// files, so you will need to decode them. If you used JSON, you can
-	// read and decode by creating a decoder and repeatedly calling
-	// .Decode(&kv) on it until it returns an error.
-	//
-	// You may find the first example in the golang sort package
-	// documentation useful.
-	//
-	// reduceF() is the application's reduce function. You should
-	// call it once per distinct key, with a slice of all the values
-	// for that key. reduceF() returns the reduced value for that key.
-	//
-	// You should write the reduce output as JSON encoded KeyValue
-	// objects to the file named outFile. We require you to use JSON
-	// because that is what the merger than combines the output
-	// from all the reduce tasks expects. There is nothing special about
-	// JSON -- it is just the marshalling format we chose to use. Your
-	// output code will look something like this:
-	//
-	// enc := json.NewEncoder(file)
-	// for key := ... {
-	// 	enc.Encode(KeyValue{key, reduceF(...)})
-	// }
-	// file.Close()
-	//
+	key_vals := make(map[string][]string)
+	for m := 0 ; m < nMap ; m++ {
+		intermediateFileName := reduceName(jobName, m, reduceTaskNumber) 
+		file, err := os.Open(intermediateFileName)
+		check_error(err)
+
+		dec := json.NewDecoder(file)
+
+		var kv KeyValue
+		for  ; true ;  {
+			err := dec.Decode(&kv)
+
+			if err != nil{
+				break 
+			}
+
+			key_vals[kv.Key] = append(key_vals[kv.Key], kv.Value)
+		}
+
+		file.Close()
+	}
+
+	file, err := os.Create(outFile)
+	check_error(err)
+	enc := json.NewEncoder(file)
+
+	for key, values := range key_vals {
+		sort.Strings(values)
+		enc.Encode(KeyValue{key, reduceF(key, values)})
+	}
+
+	file.Close()
 }
