031784064177b6d9becdbcc2ebd57368b318146e
Khaled El-Tahan <millerzbusiness@gmail.com>
2017-03-13 23:29:27 +0200

    Pass Lab2B Part IV

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -3,17 +3,9 @@ package mapreduce
 import (
 	"fmt"
 	"sync"
+	"math/rand"
 )
 
-//
-// schedule() starts and waits for all tasks in the given phase (Map
-// or Reduce). the mapFiles argument holds the names of the files that
-// are the inputs to the map phase, one per map task. nReduce is the
-// number of reduce tasks. the registerChan argument yields a stream
-// of registered workers; each item is the worker's RPC address,
-// suitable for passing to call(). registerChan will yield all
-// existing registered workers (if any) and new ones as they register.
-//
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
 	var ntasks int
 	var n_other int // number of inputs (for reduce) or outputs (for map)
@@ -27,20 +19,25 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	}
 
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
-
-	// All ntasks tasks have to be scheduled on workers, and only once all of
-	// them have been completed successfully should the function return.
-	// Remember that workers may fail, and that any given worker may finish
-	// multiple tasks.
-	//
-	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-	//
 	
 	workers := make([]string, 0)
-	var wg sync.WaitGroup
+	tasks := make(map[int]DoTaskArgs)
 
-	for i := 0 ; i < ntasks ; {
+	for i := 0 ; i < ntasks ; i++ {
+		var task_args DoTaskArgs
 
+		switch phase{
+			case mapPhase:
+				task_args = DoTaskArgs{jobName, mapFiles[i], mapPhase, i, n_other}
+			case reducePhase:
+				task_args = DoTaskArgs{jobName, "", reducePhase, i, n_other}
+		}
+
+		tasks[i] = task_args
+	}
+
+	var wg sync.WaitGroup	
+	for ended := false ; ended != true ; {
 		for flag := true ; flag ; {
 			select {
 			    case worker_address := <-registerChan:
@@ -50,26 +47,48 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	    	}
 		}
 
-		wg.Add(len(workers))
-		for j := 0 ; j < len(workers) ; j++{
-			var task_args DoTaskArgs
-
-			switch phase{
-				case mapPhase:
-					task_args = DoTaskArgs{jobName, mapFiles[i], mapPhase, i, n_other}
-				case reducePhase:
-					task_args = DoTaskArgs{jobName, "", reducePhase, i, n_other}
+		local_tasks := make([]DoTaskArgs, 0)
+		for _, v := range(tasks){
+			local_tasks = append(local_tasks,v)
+			if( len(local_tasks) == len(workers) ){
+				break
 			}
-			
-			go func(w *sync.WaitGroup, j int, workers []string, tasg_args DoTaskArgs) {
-				call(workers[j], "Worker.DoTask", task_args, nil)
-				w.Done()
-			}(&wg, j, workers, task_args)
+		}
 
-			i++
+		wg.Add(minimum(len(workers), len(local_tasks)))
+
+		//shuffle the workers
+		for i := range workers {
+			j := rand.Intn(i + 1)
+			workers[i], workers[j] = workers[j], workers[i]
+		}
+
+		for j := 0 ; j < minimum(len(workers), len(local_tasks)) ; j++{
+			
+			
+			go func(w *sync.WaitGroup, worker string, task_args DoTaskArgs, tasks *map[int]DoTaskArgs) {
+				success := call(worker, "Worker.DoTask", task_args, nil)
+				if success{
+					delete(*tasks,task_args.TaskNumber)
+				}
+				w.Done()
+			}(&wg, workers[j], local_tasks[j], &tasks)
+
 		}
 		wg.Wait()
+
+		if len(tasks) == 0 {
+			ended = true
+		}
 	}
 
 	fmt.Printf("Schedule: %v phase done\n", phase)
+}
+
+
+func minimum(a int, b int) int {
+	if a < b {
+		return a 
+	}
+	return b
 }
