50d638db70c3edb5f08a42aee26889d453c858af
Khaled El-Tahan <millerzbusiness@gmail.com>
2017-03-13 22:10:20 +0200

    Pass Lab2B Part III

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (Map
@@ -15,12 +18,12 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	var ntasks int
 	var n_other int // number of inputs (for reduce) or outputs (for map)
 	switch phase {
-	case mapPhase:
-		ntasks = len(mapFiles)
-		n_other = nReduce
-	case reducePhase:
-		ntasks = nReduce
-		n_other = len(mapFiles)
+		case mapPhase:
+			ntasks = len(mapFiles)
+			n_other = nReduce
+		case reducePhase:
+			ntasks = nReduce
+			n_other = len(mapFiles)
 	}
 
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
@@ -32,5 +35,41 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+	
+	workers := make([]string, 0)
+	var wg sync.WaitGroup
+
+	for i := 0 ; i < ntasks ; {
+
+		for flag := true ; flag ; {
+			select {
+			    case worker_address := <-registerChan:
+			        workers = append(workers, worker_address)
+			    default:
+			        flag = false
+	    	}
+		}
+
+		wg.Add(len(workers))
+		for j := 0 ; j < len(workers) ; j++{
+			var task_args DoTaskArgs
+
+			switch phase{
+				case mapPhase:
+					task_args = DoTaskArgs{jobName, mapFiles[i], mapPhase, i, n_other}
+				case reducePhase:
+					task_args = DoTaskArgs{jobName, "", reducePhase, i, n_other}
+			}
+			
+			go func(w *sync.WaitGroup, j int, workers []string, tasg_args DoTaskArgs) {
+				call(workers[j], "Worker.DoTask", task_args, nil)
+				w.Done()
+			}(&wg, j, workers, task_args)
+
+			i++
+		}
+		wg.Wait()
+	}
+
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
